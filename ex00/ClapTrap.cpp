99fb2bf74bb2edeff1db8da5b800bd9d52709ba5
atormac <atorma@student.hive.fi>
2024-10-08 15:59:33 +0300

    Dont attack with 0 energypoints

--- ex00/ClapTrap.cpp
+++ ex00/ClapTrap.cpp
@@ -4,8 +4,14 @@ ClapTrap::ClapTrap(std::string name)
 {
 	this->_name = name;
 }
+
 void ClapTrap::attack(const std::string& target)
 {
+	if (this->_energypoints == 0)
+	{
+		std::cout << "ClapTrap " << this->_name << " has no energy points to attack!" << std::endl;
+		return;
+	}
 	std::cout << "ClapTrap " << this->_name << " attacks " << target;
-	std::cout << ", causing " << "NULL" << "points of damage!" << std::endl;
+	std::cout << ", causing " << this->_attackdamage << " points of damage!" << std::endl;
 }
